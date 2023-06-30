c8f1fb031e945eeb783836f7065d753f0d670d6e
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-06-30 18:19:23 +0700

    Create auth api client

---
+++ src/auth/api.rs
@@ -0,0 +1,180 @@
+use tonic::{Request, Status, transport::Channel};
+use rmcs_auth_api::api::api_service_client::ApiServiceClient;
+use rmcs_auth_api::api::{
+    ApiSchema, ApiId, ApiName, ApiCategory, ApiUpdate,
+    ProcedureSchema, ProcedureId, ProcedureName, ProcedureUpdate
+};
+
+const API_NOT_FOUND: &str = "requested api not found";
+const PROC_NOT_FOUND: &str = "requested procedure not found";
+
+pub(crate) async fn read_api(channel: &Channel, id: u32)
+    -> Result<ApiSchema, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiId {
+        id
+    });
+    let response = client.read_api(request).await?.into_inner();
+    Ok(response.result.ok_or_else(|| Status::not_found(API_NOT_FOUND))?)
+}
+
+pub(crate) async fn read_api_by_name(channel: &Channel, name: &str)
+    -> Result<ApiSchema, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiName {
+        name: name.to_owned()
+    });
+    let response = client.read_api_by_name(request)
+        .await?
+        .into_inner();
+    Ok(response.result.ok_or_else(|| Status::not_found(API_NOT_FOUND))?)
+}
+
+pub(crate) async fn list_api_by_category(channel: &Channel, category: &str)
+    -> Result<Vec<ApiSchema>, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiCategory {
+        category: category.to_owned()
+    });
+    let response = client.list_api_by_category(request)
+        .await?
+        .into_inner();
+    Ok(response.result)
+}
+
+pub(crate) async fn create_api(channel: &Channel, name: &str, address: &str, category: &str, description: &str, password: &str)
+    -> Result<u32, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiSchema {
+        id: 0,
+        name: name.to_owned(),
+        address: address.to_owned(),
+        category: category.to_owned(),
+        description: description.to_owned(),
+        public_key: Vec::new(),
+        password: password.to_owned(),
+        access_key: Vec::new(),
+        procedures: Vec::new()
+    });
+    let response = client.create_api(request)
+        .await?
+        .into_inner();
+    Ok(response.id)
+}
+
+pub(crate) async fn update_api(channel: &Channel, id: u32, name: Option<&str>, address: Option<&str>, category: Option<&str>, description: Option<&str>, password: Option<&str>, keys: Option<()>)
+    -> Result<(), Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiUpdate {
+        id,
+        name: name.map(|s| s.to_owned()),
+        address: address.map(|s| s.to_owned()),
+        category: category.map(|s| s.to_owned()),
+        description: description.map(|s| s.to_owned()),
+        password: password.map(|s| s.to_owned()),
+        update_key: keys.is_some()
+    });
+    client.update_api(request)
+        .await?;
+    Ok(())
+}
+
+pub(crate) async fn delete_api(channel: &Channel, id: u32)
+    -> Result<(), Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiId {
+        id
+    });
+    client.delete_api(request)
+        .await?;
+    Ok(())
+}
+
+pub(crate) async fn read_procedure(channel: &Channel, id: u32)
+    -> Result<ProcedureSchema, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ProcedureId {
+        id
+    });
+    let response = client.read_procedure(request)
+        .await?
+        .into_inner();
+    Ok(response.result.ok_or_else(|| Status::not_found(PROC_NOT_FOUND))?)
+}
+
+pub(crate) async fn read_procedure_by_name(channel: &Channel, api_id: u32, name: &str)
+    -> Result<ProcedureSchema, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ProcedureName {
+        api_id,
+        name: name.to_owned()
+    });
+    let response = client.read_procedure_by_name(request)
+        .await?
+        .into_inner();
+    Ok(response.result.ok_or_else(|| Status::not_found(PROC_NOT_FOUND))?)
+}
+
+pub(crate) async fn list_procedure_by_api(channel: &Channel, api_id: u32)
+    -> Result<Vec<ProcedureSchema>, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ApiId {
+        id: api_id
+    });
+    let response = client.list_procedure_by_api(request)
+        .await?
+        .into_inner();
+    Ok(response.result)
+}
+
+pub(crate) async fn create_procedure(channel: &Channel, api_id: u32, name: &str, description: &str)
+    -> Result<u32, Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ProcedureSchema {
+        id: 0,
+        api_id,
+        name: name.to_owned(),
+        description: description.to_owned(),
+        roles: Vec::new()
+    });
+    let response = client.create_procedure(request)
+        .await?
+        .into_inner();
+    Ok(response.id)
+}
+
+pub(crate) async fn update_procedure(channel: &Channel, id: u32, name: Option<&str>, description: Option<&str>)
+    -> Result<(), Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ProcedureUpdate {
+        id,
+        name: name.map(|s| s.to_owned()),
+        description: description.map(|s| s.to_owned())
+    });
+    client.update_procedure(request)
+        .await?;
+    Ok(())
+}
+
+pub(crate) async fn delete_procedure(channel: &Channel, id: u32)
+    -> Result<(), Status>
+{
+    let mut client = ApiServiceClient::new(channel.to_owned());
+    let request = Request::new(ProcedureId {
+        id
+    });
+    client.delete_procedure(request)
+        .await?;
+    Ok(())
+}
--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,4 +1,7 @@
-use tonic::transport::Channel;
+pub mod api;
+
+use tonic::{Status, transport::Channel};
+use rmcs_auth_api::api::{ApiSchema, ProcedureSchema};
 
 #[derive(Debug, Clone)]
 pub struct Auth {
@@ -22,6 +25,85 @@ impl Auth {
         Auth {
             channel
         }
+    }
+
+    pub async fn read_api(&self, id: u32)
+        -> Result<ApiSchema, Status>
+    {
+        api::read_api(&self.channel, id).await
+    }
+
+    pub async fn read_api_by_name(&self, name: &str)
+        -> Result<ApiSchema, Status>
+    {
+        api::read_api_by_name(&self.channel, name).await
+    }
+
+    pub async fn read_procedure(&self, id: u32)
+        -> Result<ProcedureSchema, Status>
+    {
+        api::read_procedure(&self.channel, id).await
+    }
+
+    pub async fn read_procedure_by_name(&self, api_id: u32, name: &str)
+        -> Result<ProcedureSchema, Status>
+    {
+        api::read_procedure_by_name(&self.channel, api_id, name).await
+    }
+
+    pub async fn list_api_by_category(&self, category: &str)
+        -> Result<Vec<ApiSchema>, Status>
+    {
+        api::list_api_by_category(&self.channel, category).await
+    }
+
+    pub async fn create_api(&self, name: &str, address: &str, category: &str, description: &str, password: &str)
+        -> Result<u32, Status>
+    {
+        api::create_api(&self.channel, name, address, category, description, password)
+        .await
+    }
+
+    pub async fn update_api(&self, id: u32, name: Option<&str>, address: Option<&str>, category: Option<&str>, description: Option<&str>, password: Option<&str>, keys: Option<()>)
+        -> Result<(), Status>
+    {
+        api::update_api(&self.channel, id, name, address, category, description, password, keys)
+        .await
+    }
+
+    pub async fn delete_api(&self, id: u32)
+        -> Result<(), Status>
+    {
+        api::delete_api(&self.channel, id)
+        .await
+    }
+
+    pub async fn list_procedure_by_api(&self, api_id: u32)
+        -> Result<Vec<ProcedureSchema>, Status>
+    {
+        api::list_procedure_by_api(&self.channel, api_id)
+        .await
+    }
+
+    pub async fn create_procedure(&self, api_id: u32, name: &str, description: &str)
+        -> Result<u32, Status>
+    {
+        api::create_procedure(&self.channel, api_id, name, description)
+        .await
+    }
+
+    pub async fn update_procedure(&self, id: u32, name: Option<&str>, description: Option<&str>)
+        -> Result<(), Status>
+    {
+        api::update_procedure(&self.channel, id, name, description)
+        .await
+    }
+
+    pub async fn delete_procedure(&self, id: u32)
+        -> Result<(), Status>
+    {
+        api::delete_procedure(&self.channel, id)
+        .await
     }
 
 }
